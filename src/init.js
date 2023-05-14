bbc854a1af70a634a14a8ac45f37a9e64eaf66d2
Baskin Lazpberry <lazpberry1012@gmail.com>
2023-05-15 00:59:36 +0900

    Update Heroku config

--- src/init.js
+++ src/init.js
@@ -5,4 +5,4 @@ const PORT = 8000;
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+app.listen(process.env.PORT || PORT, handleListening);
