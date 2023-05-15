b71cebf3f56760bc4b71796bf5cf9d6d6998010a
Baskin Lazpberry <lazpberry1012@gmail.com>
2023-05-15 16:51:48 +0900

    Website update
    Portfolio page removed.
    Blog page added.
    Favicon added.
    Rearranged css and html.
    Replaced icon to Font Awesome

--- src/controller.js
+++ src/controller.js
@@ -3,13 +3,33 @@ export const home = (req, res) => {
 };
 
 export const portfolio = (req, res) => {
-    return res.render("portfolio.pug", { pageTitle: "portfolio", });
+    return res.render("portfolio.pug", { pageTitle: "Portfolio", 
+    index: []});
 };
 
 export const projects = (req, res) => {
-    return res.render("projects.pug", { pageTitle: "projects", });
+    return res.render("projects.pug", { pageTitle: "Projects", 
+    index: []});
+};
+export const resume = (req, res) => {
+    return res.render("resume.pug", { pageTitle: "Resume", 
+    index: ["Personal Identification", "Education", "Experience", "Skills", "References"],});
 };
 
-export const resume = (req, res) => {
-    return res.render("resume.pug", { pageTitle: "resume", });
-};
+export const blog = (req, res) => {
+    return res.render("blog.pug", { pageTitle: "Blog", 
+    index: ["Personal Identification", "Education", "Experience", "Skills", "References"],});
+}
+
+export const getWriteBlog = (req, res) => {
+    return res.render("writeblog.pug", { pageTitle: "Blog", });
+}
+
+export const postWriteBlog = (req, res) => {
+    return res.redirect("/blog");
+}
+
+export const viewBlog = (req, res) => {
+    const {id} = req.params;
+    return res.render("blog.pug", { pageTitle: "Blog", });
+}
--- src/router.js
+++ src/router.js
@@ -1,11 +1,16 @@
 import express from "express";
-import { home, portfolio, projects, resume } from "./controller";
+import { blog, getWriteBlog, home, portfolio, postWriteBlog, projects, resume, viewBlog, writeBlog } from "./controller";
 
 const router = express.Router();
 
 router.get("/", home);
 router.get("/resume", resume);
 router.get("/projects", projects);
-router.get("/portfolio", portfolio);
+
+router.get("/blog", blog);
+router.route("/blog/write").get(getWriteBlog).post(postWriteBlog);
+router.get("/blog/:id", viewBlog);
+
+//router.get("/portfolio", portfolio);
 
 export default router;
