fb3b1a3da29535d45ec9aeddd03e580bf3795659
Jeongin Yeo <102884030+LuaBerry@users.noreply.github.com>
2025-04-01 05:56:32 +0900

    Update VaultComp.js

--- src/components/VaultComp.js
+++ src/components/VaultComp.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const VaultComp = ({vault}) => {
     const navigate = useNavigate();
     return (
-        <div className="box" onClick={() => {navigate("/vault/detail", {state: {vault}})}}>
+        <div className="box" onClick={() => {/*navigate("/vault/detail", {state: {vault}})*/}}>
             <img className="thumbnail" src={vault.image} alt="vault thumbnail"></img>
             <span className="title">
                 {vault.title}
