9c1054213db223fdb83eb23c437b7ea1d8c68b06
LuaBerry <lazpberry1012@gmail.com>
2025-08-17 16:25:27 +0900

    Transferred from Heroku to local server

--- src/init.js
+++ src/init.js
@@ -1,12 +1,24 @@
+import fs from "fs";
+import https from "https";
+
 import "dotenv/config";
 import "./Chat";
 import "./Project";
 import "./db";
 import app from "./server";
 
-const PORT = 8000;
+
+const HTTPS_PORT = process.env.HTTPS_PORT || 8443;
 
 const handleListening = () =>
-    console.log(`Server listening on port http://localhost:${PORT}`);
+    console.log(`Server listening on port http://localhost:${HTTP_PORT}`);
 
-app.listen(process.env.PORT || PORT, handleListening);
+const handleListeningHttps = () =>
+    console.log(`Server listening on port https://localhost:${HTTPS_PORT}`);
+
+const httpsOptions = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+};  
+
+https.createServer(httpsOptions, app).listen(HTTPS_PORT, handleListeningHttps);
